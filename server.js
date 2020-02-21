b313644296aa986bee47aed0f05966a0a54f60d9
Yan <wangyan6783@gmail.com>
2020-02-21 12:30:11 -0600

    comment

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
+// config
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
