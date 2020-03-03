311927ae9b7e1ab14fe478d704011d52acac0a20
Yan <wangyan6783@gmail.com>
2020-03-03 15:27:33 -0600

    prettier

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,22 +11,28 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
 
 const connection = mongoose.connection;
 // config
-connection.once('open', () => {
+connection
+  .once("open", () => {
     console.log("MongoDB database connection established successfully");
-})
+  })
+  .catch(err => console.log(err));
 
-const smoothiesRouter = require('./routes/smoothies');
-const usersRouter = require('./routes/users');
-const ordersRouter = require('./routes/orders');
+const smoothiesRouter = require("./routes/smoothies");
+const usersRouter = require("./routes/users");
+const ordersRouter = require("./routes/orders");
 
-app.use('/smoothies', smoothiesRouter);
-app.use('/users', usersRouter);
-app.use('/orders', ordersRouter);
+app.use("/smoothies", smoothiesRouter);
+app.use("/users", usersRouter);
+app.use("/orders", ordersRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
